8108d391c438b5d07706171c4a3788c1b66deaf9
koreanjinoun <133323809+koreanjinoun@users.noreply.github.com>
2026-09-10 23:34:24 +0900

    Update project5-1.c

--- ch13/project5-1.c
+++ ch13/project5-1.c
@@ -1,1 +1,34 @@
+// **********************************************
+// 제 목 : 실습과제5.1
+// 날 짜 : 2026년 9월10일
+// 작성자 : 2600057 김진언
+// **********************************************
 
+#define _CRT_SECURE_NO_WARNINGS // 보안오류방지
+#pragma warning(disable:6031)  // 리턴값관련 경고 방지
+#include <stdio.h>
+
+int main(void)
+{
+    int arr[5];
+    int* ptr = arr;
+
+    printf("5개의 정수를 입력하세요: ");
+
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", ptr + i);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        *(ptr + i) += 2;
+    }
+
+    printf("2씩 증가된 배열 요소의 값: ");
+
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", *(ptr + i));
+    }
+    printf("\n");
+
+    return 0;
+}
